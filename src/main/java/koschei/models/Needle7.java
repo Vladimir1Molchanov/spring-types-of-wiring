a6b43fe1dd250c1b57672deb85ce661128fe1674
Vladimir1Molchanov <92711094+Vladimir1Molchanov@users.noreply.github.com>
2021-12-21 14:00:18 +0300

    Add files via upload

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,18 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+
+    @Autowired
+    public Needle7() {
+    }
+
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( ";
+    }
+}
